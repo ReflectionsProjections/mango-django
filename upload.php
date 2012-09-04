2719c2db8d15958c263f70fd0a20c8632d1a6441
Cole Gleason <colewashere@gmail.com>
2012-09-04 00:15:52 -0500

    some more changes

--- upload.php
+++ upload.php
@@ -9,15 +9,22 @@ if ($mysqli->connect_errno) {
 $firstname = $mysqli->real_escape_string($_POST['firstname']);
 $lastname = $mysqli->real_escape_string($_POST['lastname']);
 $netid = $mysqli->real_escape_string($_POST['netid']);
-$data = addslashes(fread(fopen($form_data, "r"), filesize($form_data)));
 
-$mysqli->query("INSERT INTO resumes (firstname, lastname, netid, file, filename, filesize, filetype) VALUES ('$firstname','$lastname','$netid','$data','$form_data_name','$form_data_size','$form_data_type')");
+$tmp_file = $_FILES['resume']['tmp_name'];
+$filesize = $_FILES['resume']['size'];
+$filename =  $_FILES['resume']['name'];
+$filetype = $_FILES['resume']['type'];
+$file_obj = fopen($tmp_file, "r");
+$data = addslashes(fread($file_obj, filesize($tmp_file)));
+fclose($file_obj);
+
+$mysqli->query("INSERT INTO resumes (firstname, lastname, netid, file, filename, filesize, filetype) VALUES ('$firstname','$lastname','$netid','$data','$filename','$filesize','$filetype')");
 $id = $mysqli->insert_id;
 $res = $mysqli->query("SELECT * FROM resumes WHERE id=".$id);
 $row = $res->fetch_assoc();
 
 print "<p>File ID: <b>$id</b><br>";
-print "<p>File Name: <b>$form_data_name</b><br>";
-print "<p>File Size: <b>$form_data_size</b><br>";
-print "<p>File Type: <b>$form_data_type</b><p>";
+print "<p>File Name: <b>$filename</b><br>";
+print "<p>File Size: <b>$filesize</b><br>";
+print "<p>File Type: <b>$filetype</b><p>";
 ?>
