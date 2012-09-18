7d501c6025af959d480e279e13e86dd1e71c989b
jtfairbank <jtfairbank@gmail.com>
2012-09-18 01:22:02 -0500

    Style Schedule w/ Alternating Rows
    Improve the HTML source by making <dt>s and <dd>s alternate.  For a <dt>
    with multiple definitions (in this case, a time with multiple events),
    put each event into its own <p> inside a common <dt>.
    
    Style the schedule w/ alternating rows, and add borders to distinguish
    between time slots.

--- schedule.php
+++ schedule.php
@@ -40,14 +40,19 @@
 						<dl class="dl-horizontal schedule">
 							<dt>10:00 AM - 4:00 PM</dt>
 							<dd>Reflections | Projections Job Fair</dd>
+
 							<dt>5:00 PM</dt>
 							<dd>Garry Tan, Partner at Y Combinator</dd>
+
 							<dt>6:30 PM</dt>
 							<dd>Dinner</dt>
+
 							<dt>7:45 PM</dt>
 							<dd>Danielle Feinberg, Director of Photography -- Lighting  at Pixar</dd>
+
 							<dt>9:00 PM</dt>
 							<dd>MechMania Opening Ceremony</dd>
+
 							<dt>9:30 PM</dt>
 							<dd>MechMania Begins</dd>
 						</dl>	
@@ -56,34 +61,57 @@
 						<dl class="dl-horizontal schedule">
 							<dt>9:00 AM</dt>
 							<dd>Breakfast</dd>
+
 							<dt>10:00 AM</dt>
-							<dd>Amber Graner, Co-Author of <em>The Official Ubuntu Book</em></dd>
-							<dd>Rian Hunter, Software Engineer at Dropbox</dd>
+							<dd>
+								<p>Amber Graner, Co-Author of <em>The Official Ubuntu Book</em></p>
+								<p>Rian Hunter, Software Engineer at Dropbox</p>
+							</dd>
+
 							<dt>11:15 AM</dt>
 							<dd>Startup Panel with Cory Levy (One), Dave Paola (Bloc), and Amit Kulkarni (Do)</dd>
+
 							<dt>12:30 AM</dt>
 							<dd>Lunch</dd>
+
 							<dt>1:00 PM - 4:00 PM</dt>
 							<dd>Reflections | Projections Startup Fair</dd>
+
 							<dt>1:30 PM</dt>
-							<dd>Shannon "JJ" Behrens, Member of Google DART Team</dd>
-							<dd>Zed Shaw, Creator of Mongrel</dd>
-							<dd>Open Hatch Talk</dd>
-							<dt>2:45 PM</dt>
-							<dd>Stefano Zacchiroli, Debian Project Leader</dd>
-							<dd>Ian Chan, Front End Engineer at Twitter</dd>
+							<dd>
+								<p>Shannon "JJ" Behrens, Member of Google DART Team</p>
+								<p>Zed Shaw, Creator of Mongrel</p>
+								<p>Open Hatch Talk</p>
+							</dd>
+
+							<dt class="col2">2:45 PM</dt>
+							<dd>
+								<p>Stefano Zacchiroli, Debian Project Leader</p>
+								<p>Ian Chan, Front End Engineer at Twitter</p>
+							</dd>
+
 							<dt>4:00 PM</dt>
-							<dd>Jiquan Ngiam, Director of Engineering at Coursera</dd>
-							<dd>Jonathan Verrecchia, Front End Engineer at Yelp</dd>
+							<dd>
+								<p>Jiquan Ngiam, Director of Engineering at Coursera</p>
+								<p>Jonathan Verrecchia, Front End Engineer at Yelp</p>
+							</dd>
+
 							<dt>5:30 PM</dt>
 							<dd>Dinner</dd>
+
 							<dt>6:30 PM</dt>
-							<dd>Andrew Ritz, Development Manager for Windows at Microsoft</dd>
-							<dd>Radu Rusu, CEO of Open Perception, Inc.</dd>
-							<dd>Google DART Workshop</dd>
-							<dt>7:45 PM</dt>
-							<dd>Joe Boutros, Project Manager at Indeed</dd>
-							<dd>Video Game Q&amp;A with Erik Wolpaw (Portal) and Steve Jaros (Volition)</dd>
+							<dd>
+								<p>Andrew Ritz, Development Manager for Windows at Microsoft</p>
+								<p>Radu Rusu, CEO of Open Perception, Inc.</p>
+								<p>Google DART Workshop</p>
+							</dd>
+
+							<dt class="odd col2">7:45 PM</dt>
+							<dd>
+								<p>Joe Boutros, Project Manager at Indeed</p>
+								<p>Video Game Q&amp;A with Erik Wolpaw (Portal) and Steve Jaros (Volition)</p>
+							</dd>
+
 							<dt>9:00 PM</dt>
 							<dd>MechMania Ends</dd>
 						</dl>
@@ -92,6 +120,7 @@
 						<dl class="dl-horizontal schedule">
 							<dt>1:00 PM</dt>
 							<dd>MechMania Closing Ceremony</dd>
+
 							<dt>TBA</dt>
 							<dd>Open Hatch Workshop</dd>
 						</dl>
