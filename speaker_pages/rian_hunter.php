de8d1e8919beaa50f92fee6a9d9f07df13f448d7
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 14:20:24 -0500

    Switched rian hunter over to new page

--- speaker_pages/rian_hunter.php
+++ speaker_pages/rian_hunter.php
@@ -1,24 +1,24 @@
-<img src="img/speaker_photos/rian_hunter.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#hunter_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Rian Hunter</h2>
-					<h4 class="speaker_job">Software Engineer at Dropbox</h4>
-				</div>
-			</a>
-		</div>
-		<div id="hunter_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Rian Hunter is Dropbox’s third engineer. He’s developed and helped shape the entire product from its early alpha and beta versions to its current form, supporting millions of users today. He’s a graduate from MIT, and his experience spans distributed computing, highly concurrent systems, databases, file systems, kernel and embedded programming, and data synchronization algorithms.</div>
-		</div>
-		<div class="accordian-heading">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#hunter_talk_abstract">
-				<h3 class="talk_title">Dropbox Notification Servers</h3>
-			</a>
-		</div>
-		<div id="hunter_talk_abstract" class="accordion-body collapse">
-      			<div class="accordion-inner talk_abstract">Dropbox aims to propagate changes across your computers as fast as possible. In most cases, as soon as a file is changed on your phone or on the web or another computer, the new data is usually available on all your computers within seconds. At the core of this system is a small number of highly-efficient and custom-built servers, each responsible for notifying millions of computers when they should be syncing with Dropbox. In this talk, you’ll hear about how Dropbox engineered this system, pushed the Linux networking stack to its absolute limit to blow the C10K problem out of the water, and came to support tens of millions of connected users with roughly a dozen servers.</div>
-		</div>
-	</div>
-</div>
+<?php
+$speaker_photo="rian_hunter.png";
+$speaker_bio_id="hunter_bio";
+$speaker_name="Rian Hunter";
+$speaker_job="Software Engineer at Dropbox";
+$speaker_bio="
+<p>
+Rian Hunter is Dropbox’s third engineer. He’s developed and helped shape the entire product from its early alpha and beta versions to its current form, supporting millions of users today. He’s a graduate from MIT, and his experience spans distributed computing, highly concurrent systems, databases, file systems, kernel and embedded programming, and data synchronization algorithms.
+</p>
+";
+
+
+$has_talk_abstract=True;
+$talk_abstract_id="hunter_talk_abstract";
+$talk_title="Dropbox Notification Servers";
+
+$talk_abstract="
+<p>
+Dropbox aims to propagate changes across your computers as fast as possible. In most cases, as soon as a file is changed on your phone or on the web or another computer, the new data is usually available on all your computers within seconds. At the core of this system is a small number of highly-efficient and custom-built servers, each responsible for notifying millions of computers when they should be syncing with Dropbox. In this talk, you’ll hear about how Dropbox engineered this system, pushed the Linux networking stack to its absolute limit to blow the C10K problem out of the water, and came to support tens of millions of connected users with roughly a dozen servers.
+</p>
+";
+
+include "speaker_template.php";
+?>
