16cae4f6aabe7b7fb80cc03c1536f277e4092da0
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 14:29:00 -0500

    Switch stefano_zacchiroli to new template

--- speaker_pages/stefano_zacchiroli.php
+++ speaker_pages/stefano_zacchiroli.php
@@ -1,26 +1,27 @@
-<img src="img/speaker_photos/stefano_zacchiroli.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#zacchiroli_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Stefano Zacchiroli</h2>
-					<h4 class="speaker_job">Debian Project Leader</h4>
-				</div>
-			</a>
-		</div>
-		<div id="zacchiroli_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Stefano Zacchiroli is an Associate Professor of Computer Science at University Paris Diderot. His research interests span formal methods and their applications to improve package quality and user experience in the context of Free Software distributions. He has been involved in the Debian Project since 2001, taking care of many tasks from package maintenance to distribution-wide Quality Assurance. He has led the Debian Project since April 2010.</div>
-		</div>
-		<div class="accordian-heading">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#zacchiroli_talk_abstract">
-				<h3 class="talk_title">Debian: 19 Years of Free Software, Do-ocracy, and Democracy</h3>
-			</a>
-		</div>
-		<div id="zacchiroli_talk_abstract" class="accordion-body collapse">
-      			<div class="accordion-inner talk_abstract">Debian is one of the oldest Free and Open Source Software (FOSS) distributions in existence. The project was founded in 1993 to further Free Software distribution, and is still doing so in a purely community-based way. The Debian Project and distribution are both made by volunteers who employ a dual "do-ocratic" (a form of meritocracy based on the outcome of individual work) and democratic model to make decisions and drive Debian toward the goal of creating the best-possible entirely Free Software operating system. The uniqueness of Debian is manifest in its Free Software values, independence from commercial interests, and in its importance as the base of a huge ecosystem of several hundreds of derived distributions, which include today's most popular GNU/Linux distributions.
+<?php
+$speaker_photo="stefano_zacchiroli.png";
+$speaker_bio_id="zacchiroli_bio";
+$speaker_name="Stefano Zacchiroli";
+$speaker_job="Debian Project Leader";
 
-In this talk, Zacchiroli will present the Debian Project and distribution, discuss its unique traits, and outline the communication challenges that a distribution entirely based on "geek" communities has to face.</div>
-		</div>
-	</div>
-</div>
+$speaker_bio="
+<p>
+Stefano Zacchiroli is an Associate Professor of Computer Science at University Paris Diderot. His research interests span formal methods and their applications to improve package quality and user experience in the context of Free Software distributions. He has been involved in the Debian Project since 2001, taking care of many tasks from package maintenance to distribution-wide Quality Assurance. He has led the Debian Project since April 2010.
+</p>
+";
+
+$has_talk_abstract=True;
+$talk_abstract_id="zacchiroli_talk_abstract";
+$talk_title="Debian: 19 Years of Free Software, Do-ocracy, and Democracy";
+
+$talk_abstract="
+<p>
+Debian is one of the oldest Free and Open Source Software (FOSS) distributions in existence. The project was founded in 1993 to further Free Software distribution, and is still doing so in a purely community-based way. The Debian Project and distribution are both made by volunteers who employ a dual \"do-ocratic\" (a form of meritocracy based on the outcome of individual work) and democratic model to make decisions and drive Debian toward the goal of creating the best-possible entirely Free Software operating system. The uniqueness of Debian is manifest in its Free Software values, independence from commercial interests, and in its importance as the base of a huge ecosystem of several hundreds of derived distributions, which include today's most popular GNU/Linux distributions.
+</p>
+<p>
+In this talk, Zacchiroli will present the Debian Project and distribution, discuss its unique traits, and outline the communication challenges that a distribution entirely based on \"geek\" communities has to face.
+</p>
+";
+
+include "speaker_template.php";
+?>
