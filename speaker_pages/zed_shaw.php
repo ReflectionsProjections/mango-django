5b4344a5e6e01379a4c1a5f2e2eea12d6ab3c4ee
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 14:35:03 -0500

    switched zed over to new template

--- speaker_pages/zed_shaw.php
+++ speaker_pages/zed_shaw.php
@@ -1,24 +1,25 @@
-<img src="img/speaker_photos/zed_shaw.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#shaw_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Zed Shaw</h2>
-					<h4 class="speaker_job">Creator of Mongrel</h4>
-				</div>
-			</a>
-		</div>
-		<div id="shaw_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Zed A. Shaw is the creator of the greatest software development methodology ever: Programming, Motherfucker. He also writes books that teach people to code, including Learn Python The Hard Way, Learn Ruby The Hard Way, and soon to be released, Learn C The Hard Way. He plays a lot of guitar, and builds them too.</div>
-		</div>
-		<div class="accordian-heading">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#shaw_talk_abstract">
-				<h3 class="talk_title">Undoing Your Student Code</h3>
-			</a>
-		</div>
-		<div id="shaw_talk_abstract" class="accordion-body collapse">
-      			<div class="accordion-inner talk_abstract">A presentation on the things that annoy everyone about beginner or "student" code, and the things you can do to avoid it. Shaw will show you what makes beginner code painful, how your code gets this way, how even professionals like Shaw still write code like this, and various tips, tricks, and techniques for making your software a pleasure to read and use. At a minimum you'll get a laugh at his expense as he shows you his own shameful "student code" and how he fixed it.</div>
-		</div>
-	</div>
-</div>
+<?php
+$speaker_photo="zed_shaw.png";
+$speaker_bio_id="shaw_bio";
+$speaker_name="Zed Shaw";
+$speaker_job="Creator of Mongrel";
+
+$speaker_bio="
+<p>
+Zed A. Shaw is the creator of the greatest software development methodology ever: Programming, Motherfucker. He also writes books that teach people to code, including Learn Python The Hard Way, Learn Ruby The Hard Way, and soon to be released, Learn C The Hard Way. He plays a lot of guitar, and builds them too.
+</p>
+
+";
+
+$has_talk_abstract=True;
+$talk_abstract_id="shaw_talk_abstract";
+$talk_title="Undoing Your Student Code";
+$talk_abstract="
+<p>
+A presentation on the things that annoy everyone about beginner or \"student\" code, and the things you can do to avoid it. Shaw will show you what makes beginner code painful, how your code gets this way, how even professionals like Shaw still write code like this, and various tips, tricks, and techniques for making your software a pleasure to read and use. At a minimum you'll get a laugh at his expense as he shows you his own shameful \"student code\" and how he fixed it.
+</p>
+";
+
+include "speaker_template.php";
+?>
+
