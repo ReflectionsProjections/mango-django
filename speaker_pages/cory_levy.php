c1d4c315ff60bbee66506092e7f83c771b7a18bb
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 13:15:22 -0500

    got the speaker templates working

--- speaker_pages/cory_levy.php
+++ speaker_pages/cory_levy.php
@@ -1,18 +1,11 @@
-<img src="img/speaker_photos/cory_levy.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#levy_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Cory Levy</h2>
-					<h4 class="speaker_job">Co-founder of One</h4>
-				</div>
-			</a>
-		</div>
-		<div id="levy_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Cory Levy is a 20 year-old entrepreneur with over five years of startup experience. At the age of 15, he worked for NutShell Mail (acquired by Constant Contact). From there he went on to do externships, while in high school, at TechStars, Union Square Ventures, DFJ Mercury, and the Founders Fund. Additionally, Levy created the NextGen Conference, a conference bringing together young entrepreneurs with more experienced entrepreneurs and VCs. Previous speakers include: Peter Thiel (Founder of PayPal), Scooter Braun (Justin Bieber’s manager), and Ashton Kutcher (attendee). As a result of Levy’s work, he has been asked to speak about entrepreneurship to university students across the country including Duke University and the University of Illinois.
+<?php
+$speaker_photo="cory_levy.png";
+$speaker_bio_id="levy_bio";
+$speaker_name="Cory Levy";
+$speaker_job="Co-founder of One";
+$speaker_bio="<p>Cory Levy is a 20 year-old entrepreneur with over five years of startup experience. At the age of 15, he worked for NutShell Mail (acquired by Constant Contact). From there he went on to do externships, while in high school, at TechStars, Union Square Ventures, DFJ Mercury, and the Founders Fund. Additionally, Levy created the NextGen Conference, a conference bringing together young entrepreneurs with more experienced entrepreneurs and VCs. Previous speakers include: Peter Thiel (Founder of PayPal), Scooter Braun (Justin Bieber’s manager), and Ashton Kutcher (attendee). As a result of Levy’s work, he has been asked to speak about entrepreneurship to university students across the country including Duke University and the University of Illinois.</p>
 
-Levy studied Computer Science at the University of Illinois. At the University of Illinois, Levy, as a freshman, was a Teachers Assistant in the Engineering Entrepreneurship course. He received four merit scholarships from the Dean of Engineering. Levy spends his time at One working in operations, technical management, and business development. He is taking a leave of absence to work full-time on One.</div>
-		</div>
-	</div>
-</div>
+<p>Levy studied Computer Science at the University of Illinois. At the University of Illinois, Levy, as a freshman, was a Teachers Assistant in the Engineering Entrepreneurship course. He received four merit scholarships from the Dean of Engineering. Levy spends his time at One working in operations, technical management, and business development. He is taking a leave of absence to work full-time on One.</p>";
+$has_talk_abstract=False;
+include "speaker_template.php";
+?>
--- speaker_pages/danielle_feinberg.php
+++ speaker_pages/danielle_feinberg.php
@@ -1,26 +1,20 @@
-<img src="img/speaker_photos/DanielleFeinberg.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#feinberg_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Danielle Feinberg</h2>
-					<h4 class="speaker_job">Director of Photography--Lighting at Pixar Animation Studios</h4>
-				</div>
-			</a>
-		</div>
-		<div id="feinberg_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Danielle Feinberg began her career at Pixar Animation Studios in February 1997, as a render technical director (also called a “render wrangler”) on the feature film, A Bug’s Life. She quickly discovered her love for lighting and went on to work as a lighting artist on many of Pixar’s feature films including, Toy Story 2, Monsters, Inc., and the Academy Award®-winning Finding Nemo, The Incredibles, and Ratatouille. Feinberg worked as the director of photography for lighting on another Oscar®-winning feature, WALL•E, and worked in the same role on the recent Disney•Pixar release, Brave.
+<?php
+$speaker_photo="DanielleFeinberg.png";
+$speaker_bio_id="feinberg_bio";
+$speaker_name="Danielle Feinberg";
+$speaker_job="Director of Photography--Lighting at Pixar Animation Studios";
+$speaker_bio="
+<p>
+Danielle Feinberg began her career at Pixar Animation Studios in February 1997, as a render technical director (also called a “render wrangler”) on the feature film, A Bug’s Life. She quickly discovered her love for lighting and went on to work as a lighting artist on many of Pixar’s feature films including, Toy Story 2, Monsters, Inc., and the Academy Award®-winning Finding Nemo, The Incredibles, and Ratatouille. Feinberg worked as the director of photography for lighting on another Oscar®-winning feature, WALL•E, and worked in the same role on the recent Disney•Pixar release, Brave. 
+</p>
+<p>
+Feinberg’s love of combining computers and art began when she was eight years old, and first programmed a Logo turtle to create images. This eventually led her to a Bachelor of Arts in Computer Science from Harvard University. Now, in addition to her Pixar work, she works with teenage girls, encouraging them to pursue math and science by demonstrating to them the same beautiful simplicity she found with the programmed art of the Logo turtle.
+</p>
+";
+$has_talk_abstract=True;
+$talk_abstract_id="feinberg_talk_abstract";
+$talk_title="The Making of Pixar's <i>Brave</i>: The Art and Algorithms Behind Movie Magic";
+$talk_abstract="What does it take to bring a computer animated film to the bring screen? It is an intriguing combination of creativity and technology. Brave was Pixar's most ambitious film to date, with a complexity level well beyond their previous movies. Feinberg and her team were tasked with creating an ancient Scottish world with telltale varied landscapes and lush, complex forests. The world was to be inhabited by a heroine with an insane head of curly red hair, a cast wearing multi-layered costumes and a slew of hairy bears. In this talk, Feinberg will take you behind the curtain, to the inner workings of their process, as well as show you some of the unique and creative solutions they found to satisfy the immense demands of this movie.";
 
-Feinberg’s love of combining computers and art began when she was eight years old, and first programmed a Logo turtle to create images. This eventually led her to a Bachelor of Arts in Computer Science from Harvard University. Now, in addition to her Pixar work, she works with teenage girls, encouraging them to pursue math and science by demonstrating to them the same beautiful simplicity she found with the programmed art of the Logo turtle.</div>
-		</div>
-		<div class="accordian-heading">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#feinberg_talk_abstract">
-				<h3 class="talk_title">The Making of Pixar's <i>Brave</i>: The Art and Algorithms Behind Movie Magic</h3>
-			</a>
-		</div>
-		<div id="feinberg_talk_abstract" class="accordion-body collapse">
-      			<div class="accordion-inner talk_abstract">What does it take to bring a computer animated film to the bring screen? It is an intriguing combination of creativity and technology. Brave was Pixar's most ambitious film to date, with a complexity level well beyond their previous movies. Feinberg and her team were tasked with creating an ancient Scottish world with telltale varied landscapes and lush, complex forests. The world was to be inhabited by a heroine with an insane head of curly red hair, a cast wearing multi-layered costumes and a slew of hairy bears. In this talk, Feinberg will take you behind the curtain, to the inner workings of their process, as well as show you some of the unique and creative solutions they found to satisfy the immense demands of this movie.</div>
-		</div>
-	</div>
-</div>
+include "speaker_template.php";
+?>
