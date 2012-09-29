259f1e813cdf03db0066386dd26866ea3dec5555
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 14:30:48 -0500

    Moved jaros to new template

--- speaker_pages/steve_jaros.php
+++ speaker_pages/steve_jaros.php
@@ -1,16 +1,16 @@
-<img src="img/speaker_photos/stevejaros.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#jaros_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Steve Jaros</h2>
-					<h4 class="speaker_job">Lead Writer for <i>Saints Row</i> (Volition)</h4>
-				</div>
-			</a>
-		</div>
-		<div id="jaros_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Steve Jaros is the Creative Director at Volition, Inc., and the Lead Writer for every Saints Row title. He enjoys pro wrestling, Disney World, the CW, and storytelling.</div>
-		</div>
-	</div>
-</div>
+<?php
+$speaker_photo="stevejaros.png";
+$speaker_bio_id="jaros_bio";
+$speaker_name="Steve Jaros";
+$speaker_job="Lead Writer for <i>Saints Row</i> (Volition)";
+
+$speaker_bio="
+<p>
+Steve Jaros is the Creative Director at Volition, Inc., and the Lead Writer for every Saints Row title. He enjoys pro wrestling, Disney World, the CW, and storytelling.
+</p>
+
+";
+
+$has_talk_abstract=False;
+include "speaker_template.php";
+?>
