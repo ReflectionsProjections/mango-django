899e1bc92fbb29775aca08cc1f05eb87d3beadb2
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 14:25:22 -0500

    moved shannon_jj_behrens to new template

--- speaker_pages/amber_graner.php
+++ speaker_pages/amber_graner.php
@@ -4,22 +4,21 @@ $speaker_bio_id="graner_bio";
 $speaker_name="Amber Graner";
 $speaker_job="Co-author of <i>The Official Ubuntu Book</i>";
 
-
-
-
-$speaker_bio="Amber Graner is currently the User Experience and Community Specialist with Linaro, LTD. In 2009, Amber Graner started her journey into Open Source via Ubuntu by blogging about her experiences. Soon she became a writer for Linux Pro and Ubuntu User Magazines, as well as co-author for the 6th and 7th Editions of The Official Ubuntu Book (Prentice Hall). Graner is also a reviewer for the 1st and 2nd Editions of The Art of Community (O'Reilly). While currently working in the Linux on ARM ecosystem, Amber is still an active Ubuntu community member and organizer who encourages everyone around her to participate, support, and learn about Ubuntu and Open Source. With a smile and a sense of humor, Amber reminds people that there is a place for everyone in the both the Linaro and Ubuntu communities--regardless of technical skill level (or lack thereof). She is constantly looking for people, places, and events within the Open Source communities that help inspire users to participate actively within the community of their choice.";
-
-
+$speaker_bio="
+<p>
+Amber Graner is currently the User Experience and Community Specialist with Linaro, LTD. In 2009, Amber Graner started her journey into Open Source via Ubuntu by blogging about her experiences. Soon she became a writer for Linux Pro and Ubuntu User Magazines, as well as co-author for the 6th and 7th Editions of The Official Ubuntu Book (Prentice Hall). Graner is also a reviewer for the 1st and 2nd Editions of The Art of Community (O'Reilly). While currently working in the Linux on ARM ecosystem, Amber is still an active Ubuntu community member and organizer who encourages everyone around her to participate, support, and learn about Ubuntu and Open Source. With a smile and a sense of humor, Amber reminds people that there is a place for everyone in the both the Linaro and Ubuntu communities--regardless of technical skill level (or lack thereof). She is constantly looking for people, places, and events within the Open Source communities that help inspire users to participate actively within the community of their choice.
+</p>
+";
 
 $has_talk_abstract=True;
 $talk_abstract_id="graner_talk_abstract";
 $talk_title="Without Fear: You Don't Need Permission to Contribute to Your Own Destiny";
 
-
-
-
-$talk_abstract="In this talk, Amber Graner will look at how applying Open Source philosophies she learned through her own contribution experience to everyday life has helped her become a stronger, more confident, and successful person in all areas of her personal, professional, and project life. Amber will share some of the most intimate moments of her Open Source, and especially Ubuntu, experiences to overcome some of her greatest fears, and will challenge the audience to to overcome their's as well. Amber will look at being open and transparent--what does meritocracy mean? Failure isn't failure, and more with her Southern wit and humor, she will challenge you to adopt these principles and become a better and more confident you."; 
-
+$talk_abstract="
+<p>
+In this talk, Amber Graner will look at how applying Open Source philosophies she learned through her own contribution experience to everyday life has helped her become a stronger, more confident, and successful person in all areas of her personal, professional, and project life. Amber will share some of the most intimate moments of her Open Source, and especially Ubuntu, experiences to overcome some of her greatest fears, and will challenge the audience to to overcome their's as well. Amber will look at being open and transparent--what does meritocracy mean? Failure isn't failure, and more with her Southern wit and humor, she will challenge you to adopt these principles and become a better and more confident you.
+</p>
+";
 
 include "speaker_template.php";
 ?>
