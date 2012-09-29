d43ab74e5b73c7c3f3b2d08f8fb26c4459630381
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 13:48:31 -0500

    switch paola over to new template. removed vestigial behrens file.

--- speaker_pages/cory_levy.php
+++ speaker_pages/cory_levy.php
@@ -4,7 +4,6 @@ $speaker_bio_id="levy_bio";
 $speaker_name="Cory Levy";
 $speaker_job="Co-founder of One";
 
-
 $speaker_bio="
 <p>
 Cory Levy is a 20 year-old entrepreneur with over five years of startup experience. At the age of 15, he worked for NutShell Mail (acquired by Constant Contact). From there he went on to do externships, while in high school, at TechStars, Union Square Ventures, DFJ Mercury, and the Founders Fund. Additionally, Levy created the NextGen Conference, a conference bringing together young entrepreneurs with more experienced entrepreneurs and VCs. Previous speakers include: Peter Thiel (Founder of PayPal), Scooter Braun (Justin Bieber’s manager), and Ashton Kutcher (attendee). As a result of Levy’s work, he has been asked to speak about entrepreneurship to university students across the country including Duke University and the University of Illinois.
@@ -14,8 +13,6 @@ Cory Levy is a 20 year-old entrepreneur with over five years of startup experien
 Levy studied Computer Science at the University of Illinois. At the University of Illinois, Levy, as a freshman, was a Teachers Assistant in the Engineering Entrepreneurship course. He received four merit scholarships from the Dean of Engineering. Levy spends his time at One working in operations, technical management, and business development. He is taking a leave of absence to work full-time on One.
 </p>
 ";
-
-
 
 $has_talk_abstract=False;
 include "speaker_template.php";
