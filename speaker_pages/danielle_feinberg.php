568938d22e0949d1ec6ddf0084621379b3de04d2
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 13:45:57 -0500

    switched ritz over to the new template

--- speaker_pages/danielle_feinberg.php
+++ speaker_pages/danielle_feinberg.php
@@ -4,17 +4,29 @@ $speaker_bio_id="feinberg_bio";
 $speaker_name="Danielle Feinberg";
 $speaker_job="Director of Photography--Lighting at Pixar Animation Studios";
 $speaker_bio="
+
+
 <p>
 Danielle Feinberg began her career at Pixar Animation Studios in February 1997, as a render technical director (also called a “render wrangler”) on the feature film, A Bug’s Life. She quickly discovered her love for lighting and went on to work as a lighting artist on many of Pixar’s feature films including, Toy Story 2, Monsters, Inc., and the Academy Award®-winning Finding Nemo, The Incredibles, and Ratatouille. Feinberg worked as the director of photography for lighting on another Oscar®-winning feature, WALL•E, and worked in the same role on the recent Disney•Pixar release, Brave. 
 </p>
+
+
 <p>
 Feinberg’s love of combining computers and art began when she was eight years old, and first programmed a Logo turtle to create images. This eventually led her to a Bachelor of Arts in Computer Science from Harvard University. Now, in addition to her Pixar work, she works with teenage girls, encouraging them to pursue math and science by demonstrating to them the same beautiful simplicity she found with the programmed art of the Logo turtle.
 </p>
+
 ";
+
+
 $has_talk_abstract=True;
 $talk_abstract_id="feinberg_talk_abstract";
 $talk_title="The Making of Pixar's <i>Brave</i>: The Art and Algorithms Behind Movie Magic";
-$talk_abstract="What does it take to bring a computer animated film to the bring screen? It is an intriguing combination of creativity and technology. Brave was Pixar's most ambitious film to date, with a complexity level well beyond their previous movies. Feinberg and her team were tasked with creating an ancient Scottish world with telltale varied landscapes and lush, complex forests. The world was to be inhabited by a heroine with an insane head of curly red hair, a cast wearing multi-layered costumes and a slew of hairy bears. In this talk, Feinberg will take you behind the curtain, to the inner workings of their process, as well as show you some of the unique and creative solutions they found to satisfy the immense demands of this movie.";
+
+$talk_abstract="
+
+What does it take to bring a computer animated film to the bring screen? It is an intriguing combination of creativity and technology. Brave was Pixar's most ambitious film to date, with a complexity level well beyond their previous movies. Feinberg and her team were tasked with creating an ancient Scottish world with telltale varied landscapes and lush, complex forests. The world was to be inhabited by a heroine with an insane head of curly red hair, a cast wearing multi-layered costumes and a slew of hairy bears. In this talk, Feinberg will take you behind the curtain, to the inner workings of their process, as well as show you some of the unique and creative solutions they found to satisfy the immense demands of this movie.
+
+";
 
 include "speaker_template.php";
 ?>
