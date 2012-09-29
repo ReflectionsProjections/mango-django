a90e2631107b9719b9c67ccddcd9624d4aa669cc
Kurtis Nusbaum <colewashere@gmail.com>
2012-09-29 13:57:01 -0500

    Switch ian chan to new template

--- speaker_pages/ian_chan.php
+++ speaker_pages/ian_chan.php
@@ -1,24 +1,26 @@
-<img src="img/speaker_photos/ian_chan.png" />
-<div class="accordian" id="accordian2">
-	<div class="accordian-group">
-		<div class="accordian-heading">
-			<a class="accordian-toggle" data-toggle="collapse" data-parent="#accordian2" href="#chan_bio">
-				<div class="speaker">
-					<h2 class="speaker_name">Ian Chan</h2>
-					<h4 class="speaker_job">Front End Engineer at Twitter</h4>
-				</div>
-			</a>
-		</div>
-		<div id="chan_bio" class="accordian-body collapse">
-			<div class="accordian-inner speaker_bio">Ian Chan is a Sr. Software Engineer at Twitter, where he has worked on the Web and Platform team for the last 2.5 years. He graduated from the University of Toronto, studying Software Engineering and Computer Science. Prior to Twitter, he helped build user interfaces and consumer experiences for Thoora, Autodesk, and IBM.</div>
-		</div>
-		<div class="accordian-heading">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#chan_talk_abstract">
-				<h3 class="talk_title">Engineering at Human Scale: Building Software for 1 to 100 million users</h3>
-			</a>
-		</div>
-		<div id="chan_talk_abstract" class="accordion-body collapse">
-      			<div class="accordion-inner talk_abstract">Building great software products for large consumer audiences is about solving big problems at huge scale, and a ton of code. As engineers, sometimes these challenges overshadow or distract us from the main goal: delivering great experiences to our users and customers. When hundreds of millions of users cause millions of lines of code to be run every second, the one in a million corner case can occur thousands of times a day. Things can break; things will break. How can we avoid, prepare for, and manage these unique, yet often inevitable, issues? This talk shares and investigates some of the more interesting corner cases encountered at Twitter over the years. Additionally Chan explores various techniques used to enable the smooth development and release of new software to millions of people around the world.</div>
-		</div>
-	</div>
-</div>
+<?php
+$speaker_photo="ian_chan.png";
+$speaker_bio_id="chan_bio";
+$speaker_name="Ian Chan";
+$speaker_job="Front End Engineer at Twitter";
+$speaker_bio="
+
+<p>
+Ian Chan is a Sr. Software Engineer at Twitter, where he has worked on the Web and Platform team for the last 2.5 years. He graduated from the University of Toronto, studying Software Engineering and Computer Science. Prior to Twitter, he helped build user interfaces and consumer experiences for Thoora, Autodesk, and IBM.
+</p>
+
+";
+
+
+$has_talk_abstract=True;
+$talk_abstract_id="chan_talk_abstract";
+$talk_title="Engineering at Human Scale: Building Software for 1 to 100 million users";
+
+$talk_abstract="
+<p>
+Building great software products for large consumer audiences is about solving big problems at huge scale, and a ton of code. As engineers, sometimes these challenges overshadow or distract us from the main goal: delivering great experiences to our users and customers. When hundreds of millions of users cause millions of lines of code to be run every second, the one in a million corner case can occur thousands of times a day. Things can break; things will break. How can we avoid, prepare for, and manage these unique, yet often inevitable, issues? This talk shares and investigates some of the more interesting corner cases encountered at Twitter over the years. Additionally Chan explores various techniques used to enable the smooth development and release of new software to millions of people around the world.
+</p>
+";
+
+include "speaker_template.php";
+?>
