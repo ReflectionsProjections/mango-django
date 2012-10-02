d0c2b10de1bc8a634ac4e841d192c096f0b8fe84
Dylan Nugent <dylnuge@gmail.com>
2012-10-02 14:33:53 -0500

    Remove EventBrite iframe from MM page
    MechMania registration is closed. Removing the EventBrite iframe so that no one can register anymore.

--- mechmania.php
+++ mechmania.php
@@ -72,14 +72,6 @@
 			</div>
 		</div>
 	</div>
-	<a name="register_jump"><div id="register" class="row mechmania-row">
-		<div class="mechmania-info span12">
-			<div class="hero-unit">
-			  <h2>Register Now!</h2>
-			  <div style="width:100%; text-align:left;" ><iframe  src="http://www.eventbrite.com/tickets-external?eid=4243049074&ref=etckt" frameborder="0" height="250" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe></div>
-			</div>
-		</div>
-	</div></a>
 	<div class="row mechmania-row">
 		<div class="mechmania-info span6">
 			<div class="hero-unit">
