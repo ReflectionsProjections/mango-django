45cdc14dda2bcd4d39adee687ac146d576854db5
Caroline Horn <chorn21@gmail.com>
2012-10-02 19:48:57 -0500

    changed links on schedule from blue to yellow, added video game q&a suggestions page

---
+++ panel_suggestions.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Video Game Q & A Question Suggestions - ACM Reflections | Projections 2012</title>
+
+	<!-- Le Styles -->
+	<link href="css/bootstrap.css" rel="stylesheet" />
+	<link href="css/rp_styles.css" rel="stylesheet" />
+	<link href="css/bootstrap-responsive.css" rel="stylesheet" />
+	<link href="css/rp_styles-responsive.css" rel="stylesheet" />
+	<!--[if !IE 7]>
+		<style type="text/css">
+			#wrap {display:table;height:100%}
+		</style>
+	<![endif]-->
+
+</head>
+<body>
+<div id="wrap">
+	<?php $page="panel_suggestions.php"; include("nav.php"); ?>
+	<div id="page-description" class="container-fluid">
+		<div class="row" id="header">
+			<div id="top-section">
+				<div class="span12">
+					<h1 class="page-heading">Video Game Q & A Question Suggestions</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="main" class="container">
+		<div class="row">
+			<div class="about-info span12">
+				<div class="hero-unit">
+				<center><iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dFRRZUNNZEtxY2dkekRBM1QzR2kyOVE6MQ" width="760" height="939" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
